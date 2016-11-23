7ee77b8a2688bd5cf75618b37a14069518284719
mchav <mchavinda@colgate.edu>
2016-11-23 04:06:47 -0500

    Linux compat

--- got.bat
+++ got.bat
@@ -6,7 +6,7 @@ SET changed=0
 if not exist %BUILD% mkdir %BUILD%
 
 REM FIXME: recompiles source unconditionally
-java -Xmx6072m -Xss10M -XX:MaxJavaStackTraceDepth=-1 -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none -cp ";%GOT_HOME%;%GOT_HOME%lib\frege3.24-7.100.jar" frege.compiler.Main -target 1.7 -d %BUILD% %GOT_HOME%src\Gottlob.fr
+java -Xmx6072m -Xss10M -XX:MaxJavaStackTraceDepth=-1 -XX:+TieredCompilation -XX:TieredStopAtLevel=1 -Xverify:none -cp ";%GOT_HOME%;%GOT_HOME%lib\*" frege.compiler.Main -target 1.7 -d %BUILD% %GOT_HOME%src\Gottlob.fr
 
 copy /b %BUILD%last.success > nul
 
